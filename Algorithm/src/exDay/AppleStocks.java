8d37eb18aa2238816058dfe53e1b4bf6700a2833
freenice12 <freenice12@gmail.com>
2016-12-19 14:25:22 +0900

    Update AppleStocks.java

--- Algorithm/src/exDay/AppleStocks.java
+++ Algorithm/src/exDay/AppleStocks.java
@@ -1,23 +1,22 @@
 public class AppleStocks {
 
     public static void main(final String[] args) {
-        final int[] stock_prices_yesterday0 = new int[] { 10, 7, 5, 8, 11, 9 }; // 6
-        final int[] stock_prices_yesterday1 = new int[] { 10, 7, 5, 8, 11, 12 }; // 7
-        final int[] stock_prices_yesterday2 = new int[] { 10, 7, 5, 8, 11, 9, 1, 10 }; // 9
-        final int[] stock_prices_yesterday3 = new int[] { 5, 5, 5 }; // 0
-        final int[] stock_prices_yesterday4 = new int[] { 10, 7, 5, 5, 8, 8 }; // 3
+        final Integer[] stock_prices_yesterday0 = new Integer[] { 10, 7, 5, 8, 11, 9 }; // 6
+        final Integer[] stock_prices_yesterday1 = new Integer[] { 10, 7, 5, 8, 11, 12 }; // 7
+        final Integer[] stock_prices_yesterday2 = new Integer[] { 10, 7, 5, 8, 11, 9, 1, 10 }; // 9
+        final Integer[] stock_prices_yesterday3 = new Integer[] { 5, 5, 5 }; // 0
+        final Integer[] stock_prices_yesterday4 = new Integer[] { 10, 7, 5, 5, 8, 8 }; // 3
 
-        final int result0 = get_max_profit(stock_prices_yesterday0);
-        final int result1 = get_max_profit(stock_prices_yesterday1);
-        final int result2 = get_max_profit(stock_prices_yesterday2);
-        final int result3 = get_max_profit(stock_prices_yesterday3);
-        final int result4 = get_max_profit(stock_prices_yesterday4);
-
-        System.out.println("resutl0: " + result0);
-        System.out.println("resutl1: " + result1);
-        System.out.println("resutl2: " + result2);
-        System.out.println("resutl2: " + result3);
-        System.out.println("resutl2: " + result4);
+        final List<List<Integer>> stocks = new ArrayList<>();
+        stocks.add(Arrays.asList(stock_prices_yesterday0));
+        stocks.add(Arrays.asList(stock_prices_yesterday1));
+        stocks.add(Arrays.asList(stock_prices_yesterday2));
+        stocks.add(Arrays.asList(stock_prices_yesterday3));
+        stocks.add(Arrays.asList(stock_prices_yesterday4));
+        for (final List<Integer> eachStocks : stocks) {
+            final int result = get_max_profit((Integer[]) eachStocks.toArray());
+            System.out.println("result: " + result);
+        }
     }
 
     private static int get_max_profit(final int[] stock_prices_yesterday) {
