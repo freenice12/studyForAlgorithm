caa3e652481935e73ef21c5ccb20063dd101867a
freenice12 <freenice12@gmail.com>
2016-12-19 14:26:56 +0900

    Update AppleStocks.java

--- Algorithm/src/exDay/AppleStocks.java
+++ Algorithm/src/exDay/AppleStocks.java
@@ -1,15 +1,11 @@
 public class AppleStocks {
 
     public static void main(final String[] args) {
-        final Integer[] stock_prices_yesterday0 = new Integer[] { 10, 7, 5, 8,
-                11, 9 }; // 6
-        final Integer[] stock_prices_yesterday1 = new Integer[] { 10, 7, 5, 8,
-                11, 12 }; // 7
-        final Integer[] stock_prices_yesterday2 = new Integer[] { 10, 7, 5, 8,
-                11, 9, 1, 10 }; // 9
+        final Integer[] stock_prices_yesterday0 = new Integer[] { 10, 7, 5, 8, 11, 9 }; // 6
+        final Integer[] stock_prices_yesterday1 = new Integer[] { 10, 7, 5, 8, 11, 12 }; // 7
+        final Integer[] stock_prices_yesterday2 = new Integer[] { 10, 7, 5, 8, 11, 9, 1, 10 }; // 9
         final Integer[] stock_prices_yesterday3 = new Integer[] { 5, 5, 5 }; // 0
-        final Integer[] stock_prices_yesterday4 = new Integer[] { 10, 7, 5, 5,
-                8, 8 }; // 3
+        final Integer[] stock_prices_yesterday4 = new Integer[] { 10, 7, 5, 5, 8, 8 }; // 3
 
         final List<List<Integer>> stocks = new ArrayList<>();
         stocks.add(Arrays.asList(stock_prices_yesterday0));
